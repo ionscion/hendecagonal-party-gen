1b264275bf684a95cb509fd52e8a254cda56abc3
bensboto <bensboto@gmail.com>
2023-03-07 20:35:38 -0700

    added post method in api to save new notes to db file

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const app = express();
 const path = require("path");
 const dbData = require("./db/db.json");
-const uuid = require("uuid");
+const uuid = require('uuid');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -21,8 +21,26 @@ app.get("/notes", (req, res) => {
 
 app.get("/api/notes", (req, res) => res.json(dbData));
 
-//todo - post method
-// app.post("/api/notes", (req, res) => res.json(dbData));
+app.post("/api/notes", (req, res) => {
+  // Get the new note from the request body
+  const newNote = req.body;
+
+  // Generate a unique ID for the new note using the uuid module
+  newNote.id = uuid.v4();
+
+  // Read the current notes from db.json
+  const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+
+  // Add the new note to the array of notes
+  currentNotes.push(newNote);
+
+  // Write the updated array of notes to db.json
+  fs.writeFileSync("./db/db.json", JSON.stringify(currentNotes));
+
+  // Send the new note back to the client
+  res.json(newNote);
+  console.info(`${req.method} request received`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
