cc875eb99de499102b6825de7de31f2810247d7e
bensboto <bensboto@gmail.com>
2023-03-07 20:48:58 -0700

    created cursory delete function, doesn't work yet

--- Develop/server.js
+++ Develop/server.js
@@ -22,24 +22,29 @@ app.get("/notes", (req, res) => {
 app.get("/api/notes", (req, res) => res.json(dbData));
 
 app.post("/api/notes", (req, res) => {
-  // Get the new note from the request body
   const newNote = req.body;
-
-  // Generate a unique ID for the new note using the uuid module
   newNote.id = uuid.v4();
-
-  // Read the current notes from db.json
   const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
-
   // Add the new note to the array of notes
   currentNotes.push(newNote);
-
   // Write the updated array of notes to db.json
   fs.writeFileSync("./db/db.json", JSON.stringify(currentNotes));
-
   // Send the new note back to the client
   res.json(newNote);
   console.info(`${req.method} request received`);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  // Get the ID of the note to delete from the request parameters
+  const noteId = req.params.id;
+  // Read the current notes from db.json
+  const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+  // Filter out the note with the specified ID
+  const updatedNotes = currentNotes.filter((note) => note.id !== noteId);
+  // Write the updated array of notes to db.json
+  fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
+  // Send a success message back to the client
+  res.json({ message: 'Note deleted successfully' });
 });
 
 app.listen(PORT, () => {
